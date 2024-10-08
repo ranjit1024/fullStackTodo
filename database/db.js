03bf304f2d2a0095bd1ffcb1c70b8f87982f0512
ranjit1024 <ranjitdas2048@gmail.com>
2024-10-08 12:57:15 +0530

    done with all the vaidation

---
+++ database/db.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://ranjit:iamranjit@cluster0.ehxzb.mongodb.net/Todo?retryWrites=true&w=majority&appName=Cluster0");
+
+const userSchema = mongoose.Schema({
+    username:String,
+    password:String,
+    email:String,
+    task:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'todo'
+        }
+    ]
+})
+
+const TodoSchema = mongoose.Schema({
+    task:String,
+    descripition:String,
+    competed:Boolean
+})
+
+const User = mongoose.model("User", userSchema);
+const Task = mongoose.model("todo", TodoSchema);
+
+module.exportes = {
+    User,
+    Task
+}
--- login/index.js
+++ login/index.js
@@ -1,22 +1,44 @@
-const signInForm = document.getElementById("signInForm");
-const signUpForm = document.getElementById("signUpForm");
-const toggleSignUp = document.getElementById("toggleSignUp");
-const toggleSignIn = document.getElementById("toggleSignIn");
-
 const signUp = document.querySelector("#signUpForm");
 
 const signupUsername = document.querySelector("#username")
 const signupPassword = document.querySelector("#password");
 const signupEmail = document.querySelector("#email");
 
+const wrong = document.querySelector(".wrong")
 
-// Switch to Sign Up Form
 
+signupUsername.addEventListener("input", async (e)=>{
 
-signUp.addEventListener("submit", async (e)=>{
-    e.preventDefault();
-    const response = await fetch(`http://192.168.2.6:3000/signup/?username=${signupUsername.value}&&password=${signupPassword.value}&&email=${signupEmail.value}`);
-    const finalResponse = await response.text();
-    console.log(signupUsername.value)
-   
+    let timeout;
+    function debouncedRequest(){
+        clearTimeout(timeout);
+        timeout = setInterval(function() {
+            validate()
+        },300)
+    }
+
+    async function validate(){
+    const response = await fetch(`http://192.168.2.6:3001/signup/?username=${signupUsername.value}&&password=${signupPassword.value}&&email=${signupEmail.value}`);
+    const finalResponse = await response.json();
+
+    let  successData = [];
+
+    finalResponse.validation.forEach(element =>{
+        successData.push(element.success);
+    })
+    // console.log(successData);
+
+    if(!successData[0]){
+        document.getElementById("username").style.border ="1.5px solid red";
+        document.querySelector(".wrongUsername").classList.remove("hide")
+    }
+    else{
+        document.getElementById("username").style.border ="1px solid #ccc";
+        document.querySelector(".wrongUsername").classList.add("hide");
+    }
+}
+    
+    
+debouncedRequest()
 })
+
